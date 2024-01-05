7d5448d88332e999823647262d0310495d7c7687
medinadelatorre <jose.medina.delatorre@gmail.com>
2024-01-05 10:37:54 -0600

    Version viernes

---
+++ app/src/main/java/com/ingeniosoft/vistas_app_produccion/aux_exitoso.java
@@ -0,0 +1,19 @@
+package com.ingeniosoft.vistas_app_produccion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class aux_exitoso extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aux_exitoso);
+
+
+
+    }
+}
---
+++ app/src/main/java/com/ingeniosoft/vistas_app_produccion/salidaalmacen.java
@@ -0,0 +1,14 @@
+package com.ingeniosoft.vistas_app_produccion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class salidaalmacen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_salidaalmacen);
+    }
+}
